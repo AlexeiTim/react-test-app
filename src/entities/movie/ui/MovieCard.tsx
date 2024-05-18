1dd638abd40da9bd72074541a1da255490b64566
AlexeiTim <86958614+AlexeiTim@users.noreply.github.com>
2024-05-18 19:57:42 +0300

    Реализовал полное отображение карточки, с различными подсчётами

--- src/entities/movie/ui/MovieCard.tsx
+++ src/entities/movie/ui/MovieCard.tsx
@@ -1,40 +1,59 @@
 import { Button, Divider, Flex, Image, Modal, Paper, Rating, Text } from "@mantine/core";
 import { useNavigate } from "react-router-dom"
 import { useDisclosure } from "@mantine/hooks";
-import image from '@/app/assets/imgs/image.png'
+import { Movie } from "../types/movie-response";
+import { Genre } from "@/entities/genres/types/genre-response";
+import { useMemo } from "react";
+import NotPoster from '@/app/assets/imgs/NotPoster.png'
 
-export const MovieCard = () => {
+interface Props {
+    movie: Movie
+    genres: Genre[]
+}
+
+export const MovieCard = ({ movie, genres }: Props) => {
     const [opened, { open, close }] = useDisclosure(false);
-
     const navigate = useNavigate()
-
 
     function handleGoToMovieDetail(id: number) {
         navigate(`/movies/${id}`)
     }
 
+    const currentGenres = useMemo(() => {
+        if (!movie.genre_ids.length) return 'Unknown'
+        return genres?.length && genres.map(g => movie.genre_ids.includes(g.id) ? g.name : '').join(' ')
+    }, [movie.genre_ids, genres])
+
+    const movieCount = useMemo(() => {
+        const calcCount = movie.vote_count / 1000
+        return calcCount > 1 ? calcCount.toFixed(2) + 'K' : movie.vote_count
+    }, [movie.vote_count])
+
     return (
         <div>
             <Paper p={24} radius="lg">
                 <Flex gap={16} direction={{ base: 'column', sm: 'row' }}>
-                    <Image src={image} onClick={() => handleGoToMovieDetail(1)} className="cursor-pointer" />
+                    <Image
+                        src={movie.backdrop_path ? `http://image.tmdb.org/t/p/w500${movie.backdrop_path}` : NotPoster}
+                        onClick={() => handleGoToMovieDetail(1)}
+                        className="cursor-pointer w-[119px] h-[170px]" />
                     <Flex justify="space-between" className="w-full">
                         <Flex direction="column" justify="space-between">
                             <Flex direction="column" gap={8}>
-                                <Text c='#9854F6' fw={700} size="20px">The Green Mile</Text>
-                                <Text c='#7B7C88' size="16px">1999</Text>
+                                <Text c='#9854F6' fw={700} size="20px">{movie.title}</Text>
+                                <Text c='#7B7C88' size="16px">{movie.release_date}</Text>
                                 <Flex gap={8} align="center">
                                     <Flex gap={4} align="center">
-                                        <Rating size={28} count={1} />
-                                        <Text fw={700}>9.3</Text>
+                                        <Rating size={28} count={1} value={1} readOnly />
+                                        <Text fw={700}>{movie.vote_average}</Text>
                                     </Flex>
-                                    <Text c='#7B7C88'>(2.9M)</Text>
+                                    <Text c='#7B7C88'>({movieCount})</Text>
                                 </Flex>
                             </Flex>
                             <Text size="16px">
                                 <Flex gap={8}>
                                     <span className="text-[#7B7C88]">Genres</span>
-                                    <span>Drama, Crime, Fantasy</span>
+                                    <span>{currentGenres}</span>
                                 </Flex>
                             </Text>
                         </Flex>
