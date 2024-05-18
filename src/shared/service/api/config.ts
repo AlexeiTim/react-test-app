26d9ae57e9055ffcf793e46761e145194b130205
AlexeiTim <86958614+AlexeiTim@users.noreply.github.com>
2024-05-19 02:02:43 +0300

    ещё поправил конфиг

--- src/shared/service/api/config.ts
+++ src/shared/service/api/config.ts
@@ -2,14 +2,7 @@ import axios from 'axios'
 
 const csrfHeader = { 'X-CSRFToken': localStorage.getItem('token') }
 
-// export function setToken(newToken: string | null) {
-//   
-//   newToken ? localStorage.setItem('token', newToken) : clearToken()
-// }
 axios.defaults.headers.common.Authorization = `Bearer ${import.meta.env.VITE_API_TOKEN}`
-function clearToken() {
-    localStorage.removeItem('token')
-}
 
 const apiBaseURL = `${import.meta.env.VITE_APP_API_URL}`
 
@@ -19,14 +12,10 @@ axios.interceptors.response.use(
     response => response,
     (error) => {
         console.error(error)
-        if (error.response.status === 401 || error.response.status === 403)
-            clearToken()
-
         return Promise.reject(error)
     },
 )
 
 export default {
     csrfHeader,
-    clearToken,
 }
