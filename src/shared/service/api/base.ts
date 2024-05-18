386695976d8e885c0fdc836658e93957f22bcf3d
AlexeiTim <86958614+AlexeiTim@users.noreply.github.com>
2024-05-19 01:14:17 +0300

    не смог удержаться, и вернул прошлую версию( привет линтеру)

--- src/shared/service/api/base.ts
+++ src/shared/service/api/base.ts
@@ -3,7 +3,8 @@ import { API_METHODS, SERVICE_METHOD_ERRORS } from '@/shared/config/service'
 import type { BaseResponse } from '@/shared/types/api'
 
 function clearEmptyParams(params: object) {
-    return Object.fromEntries(Object.entries(params).filter((array) => !!array[1] || typeof array[1] === 'boolean'))
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    return Object.fromEntries(Object.entries(params).filter(([_, value]) => !!value || typeof value === 'boolean'))
 }
 
 export class BaseApiService<T> {
