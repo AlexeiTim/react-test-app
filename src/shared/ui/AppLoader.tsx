4509b733dc608915c92a0e223f877438d5c958d5
AlexeiTim <86958614+AlexeiTim@users.noreply.github.com>
2024-05-18 19:58:17 +0300

    Добавильный глобальный компонент загрузки

---
+++ src/shared/ui/AppLoader.tsx
@@ -0,0 +1,7 @@
+import { Flex, Loader } from "@mantine/core";
+
+export const AppLoader = () => (
+    <Flex className="h-[90vh]" align="center" justify="center">
+        <Loader />
+    </Flex>
+)
