5ba3a20cab02e2670f6210989a4e591f00c1f3d5
AlexeiTim <86958614+AlexeiTim@users.noreply.github.com>
2024-05-18 19:58:07 +0300

    Времено заглушил страницу с оцененными фильмами

--- src/pages/RatedMovies/ui/RatedMoviesPage.tsx
+++ src/pages/RatedMovies/ui/RatedMoviesPage.tsx
@@ -1,14 +1,50 @@
-import { MovieCard } from "@/entities/movie"
-import { Button, Flex, Grid, Input, Pagination, Text, Image } from "@mantine/core"
-import { IconSearch } from '@tabler/icons-react';
+import { MovieCard, moviesService } from "@/entities/movie"
+import { Button, Flex, Grid, Input, Pagination, Text, Image, Notification } from "@mantine/core"
+import { IconSearch, IconX } from '@tabler/icons-react';
 import EmptyRatedImage from '@/app/assets/imgs/EmptyRatedImage.png'
+import { useEffect, useState } from "react";
+import { Movie } from "@/entities/movie/types/movie-response";
+import { Genre } from "@/entities/genres/types/genre-response";
+import { AppLoader } from "@/shared/ui/AppLoader";
+import { defineErrorMessage } from "@/shared/lib/defineErrorMessage";
+import { genresService } from "@/entities/genres/api";
 
 export const RatedMoviesPage = () => {
-    const items = Array(5).fill(null)
+    const [totalPages, setTotalPages] = useState(0)
+    const [activePage, setActivePage] = useState(1)
+    const [isLoading, setIsLoading] = useState(true)
+    const [error, setError] = useState<string | null>(null)
+    const [movies, setMovies] = useState<Movie[]>([])
+    const [genres, setGenres] = useState<Genre[]>([])
+
+
+    useEffect(() => {
+        async function getMovies() {
+            try {
+                setError(null)
+                setIsLoading(true)
+                const { data } = await moviesService.getAll({ page: activePage })
+                const { data: genresResponse } = await genresService.getAll()
+                setGenres(genresResponse.genres)
+                setMovies(data.results)
+                setTotalPages(data.total_pages > 500 ? 500 : data.total_pages)
+            } catch (e) {
+                setError(defineErrorMessage(e))
+            } finally {
+                setIsLoading(false)
+            }
+
+        }
+        getMovies()
+    }, [activePage])
+
+    if (isLoading)
+        return <AppLoader />
 
     return (
-        <>
-            {items.length ? (
+        <div>
+            {error && (<Notification icon={<IconX />} color="red" title={error} />)}
+            {movies.length ? (
                 <Flex direction="column" gap={40}>
                     <Flex align="center" justify="space-between">
                         <Text fw={600} size="32px">Rated movies</Text>
@@ -16,14 +52,14 @@ export const RatedMoviesPage = () => {
                     </Flex>
                     <Flex direction="column" gap={24}>
                         <Grid columns={12} >
-                            {items.map(() => (
+                            {movies.map((movie) => (
                                 <Grid.Col span={{ base: 12, sm: 6 }}>
-                                    <MovieCard />
+                                    <MovieCard movie={movie} genres={genres} />
                                 </Grid.Col>
                             ))}
                         </Grid>
                         <Flex justify="center">
-                            <Pagination total={3} color="grape" />
+                            <Pagination total={totalPages} value={activePage} onChange={setActivePage} color="grape" />
                         </Flex>
                     </Flex>
                 </Flex>
@@ -40,7 +76,7 @@ export const RatedMoviesPage = () => {
                     </div>
                 </Flex>
             )}
-        </>
+        </div>
     )
 }
 
